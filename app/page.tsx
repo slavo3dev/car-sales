b8b81d572aad886ef4fd95c9b04dabb24d728398
slavo3dev <slavo@slavo.io>
2026-06-12 10:00:00 -0400

    Adding Middlware

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Listings from "@/components/Listings";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Navbar from '@/components/Navbar';
+import Listings from '@/components/Listings';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Listings />
-      <Contact />
-      <Footer />
-    </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			{/* <Hero /> */}
+			<Listings />
+			<Contact />
+			<Footer />
+		</main>
+	);
 }
